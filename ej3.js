66bc47a616e58f293799e4f06bba7c9775f36b05
ivanpuebla10 <ivanpuebla94@gmail.com>
2022-01-17 16:49:00 +0100

    ejercicio 3 terminado

---
+++ ej3.js
@@ -0,0 +1,100 @@
+const members = {
+    description: 'Productos',
+    items: [
+      { id: 1, nombre: 'Taza de Harry Potter' , precio: 300},
+      { id: 2, nombre: 'FIFA 22 PS5' , precio: 1000},
+      {  id: 3, nombre: 'Figura Goku Super Saiyan' , precio: 100},
+      {  id: 4,  nombre: 'Zelda Breath of the Wild' , precio: 200},
+      {  id: 5,  nombre: 'Skin Valorant' , precio: 120},
+      {  id: 6, nombre: 'Taza de Star Wars' , precio: 220}
+    ]
+  }
+  
+
+//Punto 1
+const express = require("express");
+
+const app = express();
+
+app.listen("3000", () => {
+    console.log("Server started on port 3000");
+});
+
+//Punto 2
+// app.get('/productos', (req, res) => {
+//     res.json(members)
+// })
+
+//Punto 3
+// app.post('/productos', (req, res) => {
+//     console.log(req.body)
+//     const newMember = { nombre: 'Taza de The Bridge', precio: 1 }
+
+//     members.items.push(newMember)
+//     res.json(members)
+// });
+
+//Punto 4
+// app.put('/productos/:nombre', (req, res) => {
+//     const found = members.items.some(product => product.nombre === req.params.nombre)
+//     if (found) {
+//         for (const item of members.items) {
+//             if (item.nombre !== req.params.nombre) {
+//                 item.nombre = 'Taza del CEU'
+
+//                 res.json(members)
+//             }
+//         }
+//     } else {
+//         res.status(404).json({ msg: `Producto con el nombre ${req.params.nombre} no encontrado` })
+//     }
+// })
+
+//Punto 5
+// app.delete('/productos/:id',(req,res)=>{
+//     const found = members.items.some(product =>product.id === +req.params.id)
+    
+//     if (found) {
+//         res.json( members.items.filter(product => product.id !== +req.params.id))
+//     } else {
+//         res.status(404).json({ msg: `Producto con el id ${req.params.id} no encontrado` })
+//     }
+//   })
+
+//Punto 6
+//   app.get('/productos/:precio', (req, res) => {
+//     const found = members.items.some(product =>product.precio === +req.params.precio)
+    
+//         if (found) {
+//             res.json( members.items.filter(product => product.precio === +req.params.precio))
+//         } else {
+//             res.status(404).json({ msg: `Producto con el precio ${req.params.precio} no encontrado` })
+//         }    
+// })
+
+//Punto 7
+// app.get('/productos', (req, res) => {    
+//     res.json( members.items.filter(product => product.precio > 50 && product.precio < 250))
+// })
+
+//Punto 8
+// app.get('/productos/:id',(req,res)=>{
+//     const found = members.items.some(product =>product.id === +req.params.id)
+    
+//     if (found) {
+//         res.json( members.items.filter(product => product.id === +req.params.id))
+//     } else {
+//         res.status(404).json({ msg: `Producto con el id ${req.params.id} no encontrado` })
+//     }
+//   })
+
+//Punto 9
+// app.get('/productos/:nombre',(req,res)=>{
+//     const found = members.items.some(product =>product.nombre === req.params.nombre)
+    
+//     if (found) {
+//         res.json( members.items.filter(product => product.nombre === req.params.nombre))
+//     } else {
+//         res.status(404).json({ msg: `Producto con el nombre ${req.params.nombre} no encontrado` })
+//     }
+//   })
